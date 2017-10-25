76c86ad8a518f45af9774246ea40bf2775b6b299
qxoqx <qxoqx@qq.com>
2017-10-25 13:22:13 +0800

    vscode test

--- sigaction.c
+++ sigaction.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-//#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
 static pthread_t tid1;
 static pthread_t tid2;
 
@@ -12,39 +13,42 @@ void sig_handler(int signum, siginfo_t *info,void *c){
     pid = getpid();
     uid = getuid();
     printf("getpid = %u, getuid = %u, siginfo_pid = %u, siginfo_uid = %u\n", pid, uid, info->si_pid, info->si_uid);
-    printf("child thread tid = %u\n", pthread_self());  
+    //printf("signal thread id = 0x%x, pid = %u, tid = %u\n", pthread_self(), getpid(), syscall(SYS_gettid));  
     pthread_exit(0);
     return;
 }
 
 static void* thread_a(void *args)
 {
-    sigset_t mask;
-    sigemptyset(&mask);  
-    sigaddset(&mask, SIGSEGV); 
-    pthread_sigmask(SIG_SETMASK, &mask, NULL);
     char *cp = NULL;
+    // sigset_t mask;
+    // sigemptyset(&mask);  
+    // sigaddset(&mask, SIGSEGV); 
+    // pthread_sigmask(SIG_SETMASK, &mask, NULL);
+    
   //  printf("child thread lwpid = %u\n", syscall(SYS_gettid));  
-    printf("child thread tid = %u\n", pthread_self());  
+    //printf("child thread a thread id = 0x%x, pid = %u, tid = %u\n", pthread_self(), getpid(), syscall(SYS_gettid));  
 
-    sleep(5);
-    *cp = '\0';
+    sleep(10);
+    //*cp = '\0';
     pthread_exit(0);
     
 }
 
 static void* thread_b(void *args)
 {
-    sigset_t mask;
-    sigemptyset(&mask);  
-    sigaddset(&mask, SIGSEGV); 
-    pthread_sigmask(SIG_SETMASK, &mask, NULL);
     char *cp = NULL;
+    // sigset_t mask;
+    // sigemptyset(&mask);  
+    // sigaddset(&mask, SIGSEGV); 
+    // pthread_sigmask(SIG_SETMASK, &mask, NULL);
+    
   //  printf("child thread lwpid = %u\n", syscall(SYS_gettid));  
-    printf("child thread tid = %u\n", pthread_self());  
-
-    sleep(5);
-    *cp = '\0';
+    //printf("child thread b thread id = 0x%x, pid = %u, tid = %u\n", pthread_self(), getpid(), syscall(SYS_gettid));  
+    sleep(1);
+    //pthread_kill(tid1, SIGSEGV);
+    sleep(9);
+    //*cp = '\0';
     pthread_exit(0);
     
 }
@@ -59,14 +63,14 @@ int main(void)
     sigaction(SIGSEGV, &act, NULL);
 
     //printf("thread_id = %u, SYS_gettid = %u\n", pthread_self(), syscall(SYS_gettid));
-    printf("child thread tid = %u\n", pthread_self());  
+    //printf("child thread m thread id = 0x%x, pid = %u, tid = %u\n", pthread_self(), getpid(), syscall(SYS_gettid));  
     
     pthread_create( &tid1, 0, &thread_a, NULL );
     pthread_create( &tid2, 0, &thread_b, NULL );
     pthread_join(tid1, 0);
     pthread_join(tid2, 0);
 
-    sleep(10);
+    sleep(5);
     char *p = NULL;
     *p = 123; 
     while(1){
